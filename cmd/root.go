a97ff87fa15818c48ca33933b5e948ffea58353f
Cyril Jouve <jv.cyril@gmail.com>
2022-07-21 19:06:35 +0200

    correct --help cli arg regression
    Signed-off-by: Cyril Jouve <jv.cyril@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -121,4 +121,6 @@ func setGlobalOptionsForRootCmd(fs *pflag.FlagSet, globalOptions *config.GlobalO
 	The name of a release can be used as a label. --selector name=myrelease`)
 	fs.BoolVar(&globalOptions.AllowNoMatchingRelease, "allow-no-matching-release", globalOptions.AllowNoMatchingRelease, `Do not exit with an error code if the provided selector has no matching releases.`)
 	fs.BoolVarP(&globalOptions.Interactive, "interactive", "i", globalOptions.Interactive, "Request confirmation before attempting to modify clusters")
+	// avoid 'pflag: help requested' error (#251)
+	fs.BoolP("help", "h", false, "help for helmfile")
 }
