68d7c5f6a42e1ef583e9f21f9a60740538434577
vasicvuk <vuk.vasic@asseco-see.rs>
2022-07-26 16:20:38 +0200

    Implement readDirEntries method
    Signed-off-by: vasicvuk <vuk.vasic@asseco-see.rs>

--- pkg/tmpl/context.go
+++ pkg/tmpl/context.go
@@ -1,9 +1,12 @@
 package tmpl
 
+import "io/fs"
+
 type Context struct {
 	preRender bool
 	basePath  string
 	readFile  func(string) ([]byte, error)
+	readDir   func(string) ([]fs.DirEntry, error)
 }
 
 // SetBasePath sets the base path for the template
@@ -13,4 +16,8 @@ func (c *Context) SetBasePath(path string) {
 
 func (c *Context) SetReadFile(f func(string) ([]byte, error)) {
 	c.readFile = f
+}
+
+func (c *Context) SetReadDir(f func(string) ([]fs.DirEntry, error)) {
+	c.readDir = f
 }
