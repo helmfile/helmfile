b347eec6f2e223635f7d43053b715e8aca6069b3
yxxhero <aiopsclub@163.com>
2022-10-03 11:47:46 +0800

    add more tests
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -235,7 +235,7 @@ if [[ helm_major_version -eq 3 ]]; then
   info "Comparing yaml overwrite feature output ${yaml_overwrite_reverse} with ${feature_golden_dir}/overwritten.yaml"
   for i in $(seq 10); do
       info "Comparing build/yaml-overwrite #$i"
-      ${helmfile} -f ${dir}/issue.657.yaml template > ${yaml_overwrite_reverse} || fail "\"helmfile template\" shouldn't fail"
+      ${helmfile} -f ${dir}/issue.657.yaml template --skip-deps > ${yaml_overwrite_reverse} || fail "\"helmfile template\" shouldn't fail"
       ./yamldiff ${feature_golden_dir}/overwritten.yaml ${yaml_overwrite_reverse} || fail "\"helmfile template\" should be consistent"
       echo code=$?
   done
