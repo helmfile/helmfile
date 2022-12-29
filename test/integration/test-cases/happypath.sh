cc2450270a4e614598237a1841dcfc469fac0009
Yusuke Kuoka <ykuoka@gmail.com>
2022-12-29 08:44:07 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Use gopkg.in/yaml.v2 for Helmfile v0.x
    Signed-off-by: Yusuke Kuoka <ykuoka@gmail.com>

--- test/integration/test-cases/happypath.sh
+++ test/integration/test-cases/happypath.sh
@@ -2,22 +2,30 @@ test_start "happypath - simple rollout of httpbin chart"
 
 happypath_case_input_dir="${cases_dir}/happypath/input"
 happypath_case_output_dir="${cases_dir}/happypath/output"
+config_file="happypath.yaml"
 
-info "Diffing ${happypath_case_input_dir}/happypath.yaml"
-bash -c "${helmfile} -f ${happypath_case_input_dir}/happypath.yaml diff --detailed-exitcode; code="'$?'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile diff"
+if [[ ${HELMFILE_V1MODE} = true ]]; then
+  pushd "${happypath_case_input_dir}"
+  mv "${config_file}" "${config_file}.gotmpl"
+  config_file="${config_file}.gotmpl"
+  popd
+fi
 
-info "Diffing ${happypath_case_input_dir}/happypath.yaml without color"
-bash -c "${helmfile} -f ${happypath_case_input_dir}/happypath.yaml --no-color diff --detailed-exitcode; code="'$?'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile diff"
+info "Diffing ${happypath_case_input_dir}/${config_file}"
+bash -c "${helmfile} -f ${happypath_case_input_dir}/${config_file} diff --detailed-exitcode; code="'$?'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile diff"
 
-info "Diffing ${happypath_case_input_dir}/happypath.yaml with limited context"
-bash -c "${helmfile} -f ${happypath_case_input_dir}/happypath.yaml diff --context 3 --detailed-exitcode; code="'$?'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile diff"
+info "Diffing ${happypath_case_input_dir}/${config_file} without color"
+bash -c "${helmfile} -f ${happypath_case_input_dir}/${config_file} --no-color diff --detailed-exitcode; code="'$?'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile diff"
 
-info "Diffing ${happypath_case_input_dir}/happypath.yaml with altered output"
-bash -c "${helmfile} -f ${happypath_case_input_dir}/happypath.yaml diff --output simple --detailed-exitcode; code="'$?'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile diff"
+info "Diffing ${happypath_case_input_dir}/${config_file} with limited context"
+bash -c "${helmfile} -f ${happypath_case_input_dir}/${config_file} diff --context 3 --detailed-exitcode; code="'$?'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile diff"
 
-info "Templating ${happypath_case_input_dir}/happypath.yaml"
+info "Diffing ${happypath_case_input_dir}/${config_file} with altered output"
+bash -c "${helmfile} -f ${happypath_case_input_dir}/${config_file} diff --output simple --detailed-exitcode; code="'$?'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile diff"
+
+info "Templating ${happypath_case_input_dir}/${config_file}"
 rm -rf ${dir}/tmp
-${helmfile} -f ${happypath_case_input_dir}/happypath.yaml --debug template --output-dir tmp
+${helmfile} -f ${happypath_case_input_dir}/${config_file} --debug template --output-dir tmp
 code=$?
 [ ${code} -eq 0 ] || fail "unexpected exit code returned by helmfile template: ${code}"
 for output in $(ls -d ${dir}/tmp/*); do
@@ -30,39 +38,39 @@ for output in $(ls -d ${dir}/tmp/*); do
     done
 done
 
-info "Applying ${happypath_case_input_dir}/happypath.yaml"
-bash -c "${helmfile} -f ${happypath_case_input_dir}/happypath.yaml apply --detailed-exitcode; code="'$?'"; echo Code: "'$code'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile apply"
+info "Applying ${happypath_case_input_dir}/${config_file}"
+bash -c "${helmfile} -f ${happypath_case_input_dir}/${config_file} apply --detailed-exitcode; code="'$?'"; echo Code: "'$code'"; [ "'${code}'" -eq 2 ]" || fail "unexpected exit code returned by helmfile apply"
 
-info "Syncing ${happypath_case_input_dir}/happypath.yaml"
-${helmfile} -f ${happypath_case_input_dir}/happypath.yaml sync
+info "Syncing ${happypath_case_input_dir}/${config_file}"
+${helmfile} -f ${happypath_case_input_dir}/${config_file} sync
 wait_deploy_ready httpbin-httpbin
 retry 5 "curl --fail $(minikube service --url --namespace=${test_ns} httpbin-httpbin)/status/200"
 [ ${retry_result} -eq 0 ] || fail "httpbin failed to return 200 OK"
 
-info "Applying ${happypath_case_input_dir}/happypath.yaml"
-${helmfile} -f ${happypath_case_input_dir}/happypath.yaml apply --detailed-exitcode
+info "Applying ${happypath_case_input_dir}/${config_file}"
+${helmfile} -f ${happypath_case_input_dir}/${config_file} apply --detailed-exitcode
 code=$?
 [ ${code} -eq 0 ] || fail "unexpected exit code returned by helmfile apply: want 0, got ${code}"
 
 info "Locking dependencies"
-${helmfile} -f ${happypath_case_input_dir}/happypath.yaml deps
+${helmfile} -f ${happypath_case_input_dir}/${config_file} deps
 code=$?
 [ ${code} -eq 0 ] || fail "unexpected exit code returned by helmfile deps: ${code}"
 
-info "Applying ${happypath_case_input_dir}/happypath.yaml with locked dependencies"
-${helmfile} -f ${happypath_case_input_dir}/happypath.yaml apply
+info "Applying ${happypath_case_input_dir}/${config_file} with locked dependencies"
+${helmfile} -f ${happypath_case_input_dir}/${config_file} apply
 code=$?
 [ ${code} -eq 0 ] || fail "unexpected exit code returned by helmfile apply: ${code}"
 ${helm} list --namespace=${test_ns} || fail "unable to list releases"
 
 info "Deleting release"
-${helmfile} -f ${happypath_case_input_dir}/happypath.yaml delete
+${helmfile} -f ${happypath_case_input_dir}/${config_file} delete
 ${helm} status --namespace=${test_ns} httpbin &> /dev/null && fail "release should not exist anymore after a delete"
 
 info "Ensuring \"helmfile delete\" doesn't fail when no releases installed"
-${helmfile} -f ${happypath_case_input_dir}/happypath.yaml delete || fail "\"helmfile delete\" shouldn't fail when there are no installed releases"
+${helmfile} -f ${happypath_case_input_dir}/${config_file} delete || fail "\"helmfile delete\" shouldn't fail when there are no installed releases"
 
 info "Ensuring \"helmfile template\" output does contain only YAML docs"
-(${helmfile} -f ${happypath_case_input_dir}/happypath.yaml template | kubectl apply -f -) || fail "\"helmfile template | kubectl apply -f -\" shouldn't fail"
+(${helmfile} -f ${happypath_case_input_dir}/${config_file} template | kubectl apply -f -) || fail "\"helmfile template | kubectl apply -f -\" shouldn't fail"
 
 test_pass "happypath"
