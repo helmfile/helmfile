37c213210eb64726a4315f5979cb47790b3ea510
yxxhero <11087727+yxxhero@users.noreply.github.com>
2023-02-12 20:15:57 +0800

    bump helm to 3.11.1 (#695)

--- pkg/app/init.go
+++ pkg/app/init.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	HelmRequiredVersion           = "v3.10.3"
-	HelmRecommendedVersion        = "v3.11.0"
+	HelmRecommendedVersion        = "v3.11.1"
 	HelmDiffRecommendedVersion    = "v3.4.0"
 	HelmSecretsRecommendedVersion = "v4.1.1"
 	HelmGitRecommendedVersion     = "v0.12.0"
