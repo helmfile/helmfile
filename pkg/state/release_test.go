1781ac6a8ce8eb8069d84a02130476488f0ca3f0
yxxhero <aiopsclub@163.com>
2023-04-22 20:52:05 +0800

    feat: add go-getter suppport for ad-hoc
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/state/release_test.go
+++ pkg/state/release_test.go
@@ -43,3 +43,60 @@ func TestExecuteTemplateExpressions(t *testing.T) {
 	require.NoErrorf(t, err, "failed to execute template expressions: %v", err)
 	require.Equalf(t, result.ValuesTemplate[0].(map[string]interface{})["fullnameOverride"], "foo", "failed to execute template expressions")
 }
+
+func TestCacheDir(t *testing.T) {
+	tests := []struct {
+		name string
+		rs   ReleaseSpec
+		want string
+	}{
+		{
+			name: "empty",
+			rs:   ReleaseSpec{},
+			want: "",
+		},
+		{
+			name: "namespace",
+			rs:   ReleaseSpec{Namespace: "baz"},
+			want: "baz",
+		},
+		{
+			name: "kubeContext",
+			rs:   ReleaseSpec{KubeContext: "qux"},
+			want: "qux",
+		},
+		{
+			name: "name",
+			rs:   ReleaseSpec{Name: "foo"},
+			want: "foo",
+		},
+		{
+			name: "namespace and kubeContext",
+			rs:   ReleaseSpec{Namespace: "baz", KubeContext: "qux"},
+			want: "baz/qux",
+		},
+		{
+			name: "namespace and name",
+			rs:   ReleaseSpec{Namespace: "baz", Name: "foo"},
+			want: "baz/foo",
+		},
+		{
+			name: "kubeContext and name",
+			rs:   ReleaseSpec{KubeContext: "qux", Name: "foo"},
+			want: "qux/foo",
+		},
+		{
+			name: "namespace and kubeContext and name",
+			rs:   ReleaseSpec{Namespace: "baz", KubeContext: "qux", Name: "foo"},
+			want: "baz/qux/foo",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.rs.CacheDir(); got != tt.want {
+				t.Errorf("ReleaseSpec.CacheDir() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
