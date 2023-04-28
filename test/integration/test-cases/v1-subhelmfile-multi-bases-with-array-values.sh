0d676ddb7bc9b7ee67781d3165e971c017214fd9
yxxhero <aiopsclub@163.com>
2023-04-28 19:52:02 +0800

    fix tests
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/test-cases/v1-subhelmfile-multi-bases-with-array-values.sh
+++ test/integration/test-cases/v1-subhelmfile-multi-bases-with-array-values.sh
@@ -3,14 +3,14 @@ if [[ ${HELMFILE_V1MODE} = true ]]; then
   v1_subhelmfile_multi_bases_with_array_values_output_dir="${cases_dir}/v1-subhelmfile-multi-bases-with-array-values/output"
 
   yaml_overwrite_tmp=$(mktemp -d)
-  yaml_overwrite_reverse=${yaml_overwrite_tmp}/yaml.template.build.yaml
+  yaml_overwrite_reverse=${yaml_overwrite_tmp}/helmfile_template_result
 
   test_start "v1 subhelmfile multi bases with array values"
   info "Comparing v1 subhelmfile multi bases with array values output ${yaml_overwrite_reverse} with ${v1_subhelmfile_multi_bases_with_array_values_output_dir}/result"
   for i in $(seq 10); do
       info "Comparing build/v1-subhelmfile-multi-bases-with-array-values #$i"
       ${helmfile} -f ${v1_subhelmfile_multi_bases_with_array_values_input_dir}/helmfile.yaml.gotmpl template -e dev &> ${yaml_overwrite_reverse} || fail "\"helmfile template\" shouldn't fail"
-      diff -u ${v1_subhelmfile_multi_bases_with_array_values_output_dir}/result.yaml ${yaml_overwrite_reverse} || fail "\"helmfile template\" should be consistent"
+      diff -u ${v1_subhelmfile_multi_bases_with_array_values_output_dir}/result ${yaml_overwrite_reverse} || fail "\"helmfile template\" should be consistent"
       echo code=$?
   done
   test_pass "v1 subhelmfile multi bases with array values"
