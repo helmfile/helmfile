a3c1e326761be1cd5e33c88c503ae4edc59831e4
yxxhero <11087727+yxxhero@users.noreply.github.com>
2023-07-16 17:22:18 +0800

    bump helm to v3.12.2 (#933)
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/app/init.go
+++ pkg/app/init.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	HelmRequiredVersion           = "v3.10.3"
-	HelmRecommendedVersion        = "v3.12.1"
+	HelmRecommendedVersion        = "v3.12.2"
 	HelmDiffRecommendedVersion    = "v3.8.1"
 	HelmSecretsRecommendedVersion = "v4.1.1"
 	HelmGitRecommendedVersion     = "v0.12.0"
