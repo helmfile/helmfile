803ff8d0abbeca82ae0d78fdeb51a115773fb153
yxxhero <11087727+yxxhero@users.noreply.github.com>
2023-08-23 10:00:45 +0800

    feat keyring supportting (#984)
    * feat keyring supportting
    
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/state/temp_test.go
+++ pkg/state/temp_test.go
@@ -38,39 +38,39 @@ func TestGenerateID(t *testing.T) {
 	run(testcase{
 		subject: "baseline",
 		release: ReleaseSpec{Name: "foo", Chart: "incubator/raw"},
-		want:    "foo-values-7884dc8d7c",
+		want:    "foo-values-6749dfb776",
 	})
 
 	run(testcase{
 		subject: "different bytes content",
 		release: ReleaseSpec{Name: "foo", Chart: "incubator/raw"},
 		data:    []byte(`{"k":"v"}`),
-		want:    "foo-values-67d6bbf498",
+		want:    "foo-values-84f9645959",
 	})
 
 	run(testcase{
 		subject: "different map content",
 		release: ReleaseSpec{Name: "foo", Chart: "incubator/raw"},
 		data:    map[string]any{"k": "v"},
-		want:    "foo-values-5d86d867b",
+		want:    "foo-values-7774fccb4f",
 	})
 
 	run(testcase{
 		subject: "different chart",
 		release: ReleaseSpec{Name: "foo", Chart: "stable/envoy"},
-		want:    "foo-values-5c47fc4b6d",
+		want:    "foo-values-5cdf68c495",
 	})
 
 	run(testcase{
 		subject: "different name",
 		release: ReleaseSpec{Name: "bar", Chart: "incubator/raw"},
-		want:    "bar-values-7c87d9b8b",
+		want:    "bar-values-758d78db87",
 	})
 
 	run(testcase{
 		subject: "specific ns",
 		release: ReleaseSpec{Name: "foo", Chart: "incubator/raw", Namespace: "myns"},
-		want:    "myns-foo-values-7fbc456bb4",
+		want:    "myns-foo-values-6888c4f5bf",
 	})
 
 	for id, n := range ids {
