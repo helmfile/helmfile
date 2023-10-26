9b9fa262631843a568221471e5339af985ab5a19
yxxhero <aiopsclub@163.com>
2023-10-26 21:40:25 +0800

    fix tests
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/test-cases/diff-args.sh
+++ test/integration/test-cases/diff-args.sh
@@ -10,11 +10,11 @@ test_start "$case_title"
 info "Comparing ${case_title} for output ${diff_args_reverse} with ${diff_args_output_dir}/overwritten.yaml"
 for i in $(seq 10); do
     info "Comparing diff-args diff debug log #$i"
-    ${helmfile} -f ${diff_args_input_dir}/helmfile.yaml diff --debug > ${diff_args_reverse} || fail "\"helmfile template\" shouldn't fail"
-    diff -u ${diff_args_output_dir}/diff.log ${diff_args_reverse} || fail "\"helmfile diff\" should be consistent"
+    ${helmfile} -f ${diff_args_input_dir}/helmfile.yaml diff --debug > ${diff_args_reverse} || fail "\"helmfile diff\" shouldn't fail"
+    diff -u ${diff_args_output_dir}/diff ${diff_args_reverse} || fail "\"helmfile diff\" should be consistent"
     cat ${diff_args_reverse}
     echo code=$?
 done
-${helmfile} -f ${diff_args_input_dir}/helmfile.yaml apply --debug > ${diff_args_reverse} || fail "\"helmfile template\" shouldn't fail"
+${helmfile} -f ${diff_args_input_dir}/helmfile.yaml apply --debug > ${diff_args_reverse} || fail "\"helmfile apply\" shouldn't fail"
 cat ${diff_args_reverse}
 test_pass "cli overwrite environment values for v1"
