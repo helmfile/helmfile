d3ad292d5d2750ca15fa889b4c84b2d41da783d9
Virginia Tavares <briosovirginia@gmail.com>
2023-11-30 02:21:51 +0100

    Create DeleteWait and DeleteTimeout parameters
    Signed-off-by: Virginia Tavares <briosovirginia@gmail.com>

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -192,6 +192,10 @@ type HelmSpec struct {
 	DisableOpenAPIValidation *bool `yaml:"disableOpenAPIValidation,omitempty"`
 	// InsecureSkipTLSVerify is true if the TLS verification should be skipped when fetching remote chart
 	InsecureSkipTLSVerify bool `yaml:"insecureSkipTLSVerify,omitempty"`
+	// Wait, if set to true, will wait until all resources are deleted before mark delete command as successful
+	DeleteWait *bool `yaml:"deleteWait,omitempty"`
+	// Timeout is the time in seconds to wait for helmfile delete command (default 300)
+	DeleteTimeout *int `yaml:"deleteTimeout,omitempty"`
 }
 
 // RepositorySpec that defines values for a helm repo
@@ -370,6 +374,11 @@ type ReleaseSpec struct {
 
 	// SuppressDiff skip the helm diff output. Useful for charts which produces large not helpful diff.
 	SuppressDiff *bool `yaml:"suppressDiff,omitempty"`
+
+	// Wait, if set to true, will wait until all resources are deleted before mark delete command as successful
+	DeleteWait *bool `yaml:"deleteWait,omitempty"`
+	// Timeout is the time in seconds to wait for helmfile delete command (default 300)
+	DeleteTimeout *int `yaml:"deleteTimeout,omitempty"`
 }
 
 func (r *Inherits) UnmarshalYAML(unmarshal func(any) error) error {
@@ -797,6 +806,17 @@ func (st *HelmState) DeleteReleasesForSync(affectedReleases *AffectedReleases, h
 					relErr = newReleaseFailedError(release, err)
 				} else {
 					var args []string
+					if release.DeleteWait != nil && *release.DeleteWait || release.DeleteWait == nil {
+						args = append(args, "--wait")
+					}
+					if release.DeleteTimeout != nil {
+						timeout := *release.DeleteTimeout
+						if timeout != 0 {
+							duration := strconv.Itoa(timeout)
+							duration += "s"
+							args = append(args, "--timeout", duration)
+						}
+					}
 					if release.Namespace != "" {
 						args = append(args, "--namespace", release.Namespace)
 					}
@@ -1047,6 +1067,9 @@ type ChartPrepareOptions struct {
 	Concurrency            int
 	KubeVersion            string
 	Set                    []string
+	// Delete wait
+	DeleteWait    bool
+	DeleteTimeout int
 }
 
 type chartPrepareResult struct {
@@ -2066,7 +2089,17 @@ func (st *HelmState) DeleteReleases(affectedReleases *AffectedReleases, helm hel
 		if release.Namespace != "" {
 			flags = append(flags, "--namespace", release.Namespace)
 		}
-
+		if release.DeleteWait != nil && *release.DeleteWait || release.DeleteWait == nil {
+			flags = append(flags, "--wait")
+		}
+		if release.DeleteTimeout != nil {
+			timeout := *release.DeleteTimeout
+			if timeout != 0 {
+				duration := strconv.Itoa(timeout)
+				duration += "s"
+				flags = append(flags, "--timeout", duration)
+			}
+		}
 		context := st.createHelmContext(&release, workerIndex)
 
 		start := time.Now()
