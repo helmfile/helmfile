ad37a34a4f396d9ce52390333c5f3d95a5952d9c
yxxhero <aiopsclub@163.com>
2023-12-03 15:14:14 +0800

    fix typo
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/test-cases/include-template-func.sh
+++ test/integration/test-cases/include-template-func.sh
@@ -21,6 +21,7 @@ test_start "include_template_func template"
 info "Comparing include_template_func template output ${include_template_func_template_reverse} with ${include_template_func_case_output_dir}/result.yaml"
 for i in $(seq 10); do
     info "Comparing build/include_template_func-template #$i"
+    ${helmfile} -f ${include_template_func_case_input_dir}/${config_file} template --concurrency 1
     ${helmfile} -f ${include_template_func_case_input_dir}/${config_file} template --concurrency 1 &> ${include_template_func_template_reverse} || fail "\"helmfile template\" shouldn't fail"
     diff -u  ${include_template_func_template_out_file} ${include_template_func_template_reverse} || fail "\"helmfile template\" should be consistent"
     echo code=$?
