e89fa706d43f4b3baa6c0f1672e1f368421bcc11
yxxhero <11087727+yxxhero@users.noreply.github.com>
2024-04-06 22:04:38 +0800

    update deps versions (#1446)

--- pkg/app/init.go
+++ pkg/app/init.go
@@ -20,9 +20,9 @@ const (
 	HelmRequiredVersion           = "v3.13.3"
 	HelmRecommendedVersion        = "v3.14.3"
 	HelmDiffRecommendedVersion    = "v3.9.5"
-	HelmSecretsRecommendedVersion = "v4.1.1"
-	HelmGitRecommendedVersion     = "v0.12.0"
-	HelmS3RecommendedVersion      = "v0.14.0"
+	HelmSecretsRecommendedVersion = "v4.6.0"
+	HelmGitRecommendedVersion     = "v0.15.1"
+	HelmS3RecommendedVersion      = "v0.16.0"
 	HelmInstallCommand            = "https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
 )
 
