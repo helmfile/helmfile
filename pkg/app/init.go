9294a4b99b729a2f1ea48710fcbebdb7f41d3e44
yxxhero <aiopsclub@163.com>
2024-05-26 20:43:56 +0800

    bump helm-diff to 3.9.7
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/app/init.go
+++ pkg/app/init.go
@@ -19,7 +19,7 @@ import (
 const (
 	HelmRequiredVersion           = "v3.14.4"
 	HelmRecommendedVersion        = "v3.15.1"
-	HelmDiffRecommendedVersion    = "v3.9.6"
+	HelmDiffRecommendedVersion    = "v3.9.7"
 	HelmSecretsRecommendedVersion = "v4.6.0"
 	HelmGitRecommendedVersion     = "v0.15.1"
 	HelmS3RecommendedVersion      = "v0.16.0"
