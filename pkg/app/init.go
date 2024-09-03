d9eb271ab71600e7c6ea659c3c5c85804c0dfbc6
yxxhero <11087727+yxxhero@users.noreply.github.com>
2024-09-03 09:05:22 +0800

    feat: upgrade helm-diff plugin version to 3.9.10 (#1688)

--- pkg/app/init.go
+++ pkg/app/init.go
@@ -19,7 +19,7 @@ import (
 const (
 	HelmRequiredVersion           = "v3.14.4"
 	HelmRecommendedVersion        = "v3.15.4"
-	HelmDiffRecommendedVersion    = "v3.9.8"
+	HelmDiffRecommendedVersion    = "v3.9.10"
 	HelmSecretsRecommendedVersion = "v4.6.0"
 	HelmGitRecommendedVersion     = "v0.15.1"
 	HelmS3RecommendedVersion      = "v0.16.0"
