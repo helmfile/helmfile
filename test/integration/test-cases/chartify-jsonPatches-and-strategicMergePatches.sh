e2bae90330364e32dab6af1fef4d8502c2b5e2de
yxxhero <aiopsclub@163.com>
2024-09-07 10:59:27 +0800

    add testcases
    Signed-off-by: yxxhero <aiopsclub@163.com>

---
+++ test/integration/test-cases/chartify-jsonPatches-and-strategicMergePatches.sh
@@ -0,0 +1,23 @@
+chartify_jsonPatches_and_strategicMergePatches_case_input_dir="${cases_dir}/chartify_jsonPatches_and_strategicMergePatches/input"
+chartify_jsonPatches_and_strategicMergePatches_case_output_dir="${cases_dir}/chartify_jsonPatches_and_strategicMergePatches/output"
+
+config_file="helmfile.yaml"
+if [[ ${HELMFILE_V1MODE} = true ]]; then
+  pushd "${chartify_jsonPatches_and_strategicMergePatches_case_input_dir}"
+  mv "${config_file}" "${config_file}.gotmpl"
+  config_file="${config_file}.gotmpl"
+  popd
+fi
+
+chartify_jsonPatches_and_strategicMergePatches_tmp=$(mktemp -d)
+chartify_jsonPatches_and_strategicMergePatches_template_reverse=${chartify_jsonPatches_and_strategicMergePatches_tmp}/chartify_jsonPatches_and_strategicMergePatches.template.log
+
+
+test_start "helmfile template with chartify_jsonPatches_and_strategicMergePatches"
+
+info "Comparing template/chartify_jsonPatches_and_strategicMergePatches"
+${helmfile} -f ${chartify_jsonPatches_and_strategicMergePatches_case_input_dir}/${config_file} template > ${chartify_jsonPatches_and_strategicMergePatches_template_reverse} || fail "\"helmfile template\" shouldn't fail"
+cat ${chartify_jsonPatches_and_strategicMergePatches_template_reverse}
+./dyff between -bs ${chartify_jsonPatches_and_strategicMergePatches_case_output_dir}/template ${chartify_jsonPatches_and_strategicMergePatches_template_reverse} || fail "\"helmfile template\" should be consistent"
+
+test_pass "helmfile template with chartify_jsonPatches_and_strategicMergePatches"
