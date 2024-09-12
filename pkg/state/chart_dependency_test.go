e8642c4ae714bf98e0a28b009eb6b6210c651f1f
Yusuke Kuoka <ykuoka@gmail.com>
2024-09-12 04:26:52 +0000

    Some code cleanup

--- pkg/state/chart_dependency_test.go
+++ pkg/state/chart_dependency_test.go
@@ -76,6 +76,65 @@ func TestGetUnresolvedDependenciess(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "duplicate charts are differentiated by alias",
+			helmState: &HelmState{
+				FilePath: "helmfile.yaml",
+				ReleaseSetSpec: ReleaseSetSpec{
+					Releases: []ReleaseSpec{
+						{
+							Name:      "foo",
+							Chart:     "myrepo/abc",
+							Version:   "> 0.2.0",
+							Namespace: "ns1",
+						},
+						{
+							Name:      "bar",
+							Chart:     "myrepo/abc",
+							Version:   "0.1.0",
+							Namespace: "ns2",
+						},
+						{
+							Name:    "baz",
+							Chart:   "myrepo/abc",
+							Version: "0.3.0",
+						},
+					},
+					Repositories: []RepositorySpec{
+						{
+							Name: "myrepo",
+							URL:  "localhost:5000/aaa",
+						},
+					},
+				},
+			},
+			wantErr:    false,
+			expectfile: "helmfile",
+			expectDeps: &UnresolvedDependencies{
+				deps: map[string][]unresolvedChartDependency{
+					"abc": {
+						{
+							ChartName:         "abc",
+							Repository:        "localhost:5000/aaa",
+							VersionConstraint: "> 0.2.0",
+							Alias:             "ns1-foo",
+						},
+						{
+							ChartName:         "abc",
+							Repository:        "localhost:5000/aaa",
+							VersionConstraint: "0.1.0",
+							Alias:             "ns2-bar",
+						},
+						{
+							ChartName:         "abc",
+							Repository:        "localhost:5000/aaa",
+							VersionConstraint: "0.3.0",
+							Alias:             "-baz",
+						},
+					},
+				},
+			},
+		},
 	}
 
 	for _, tt := range tests {
@@ -88,118 +147,6 @@ func TestGetUnresolvedDependenciess(t *testing.T) {
 			}
 			require.Equalf(t, tt.expectfile, f, "getUnresolvedDependenciess() expect file %s, got %s", tt.expectfile, f)
 			require.Equalf(t, tt.expectDeps, ds, "getUnresolvedDependenciess() expect deps %v, got %v", tt.expectDeps, ds)
-		})
-	}
-}
-
-func TestContains(t *testing.T) {
-	tests := []struct {
-		name     string
-		dep      unresolvedChartDependency
-		deps     map[string][]unresolvedChartDependency
-		expected bool
-	}{
-		{
-			name: "existing dependency with right item",
-			dep: unresolvedChartDependency{
-				ChartName:         "abc",
-				Repository:        "oci://localhost:5000/aaa",
-				VersionConstraint: "0.1.0",
-				Alias:             "abc-abc",
-			},
-			deps: map[string][]unresolvedChartDependency{
-				"abc": {
-					{
-						ChartName:         "abc",
-						Repository:        "oci://localhost:5000/aaa",
-						VersionConstraint: "0.1.0",
-						Alias:             "abc-abc",
-					},
-				},
-			},
-			expected: true,
-		},
-		{
-			name: "existing dependency but empty item",
-			dep: unresolvedChartDependency{
-				ChartName:         "abc",
-				Repository:        "oci://localhost:5000/aaa",
-				VersionConstraint: "0.1.0",
-				Alias:             "abc-abc",
-			},
-			deps: map[string][]unresolvedChartDependency{
-				"abc": {
-					{
-						ChartName:  "abc",
-						Repository: "oci://localhost:5000/aaa",
-						Alias:      "abc-abc",
-					},
-				},
-			},
-			expected: false,
-		},
-		{
-			name: "different chart version",
-			dep: unresolvedChartDependency{
-				ChartName:         "abc",
-				Repository:        "oci://localhost:5000/aaa",
-				VersionConstraint: "0.1.0",
-				Alias:             "abc-abc",
-			},
-			deps: map[string][]unresolvedChartDependency{
-				"abc": {
-					{
-						ChartName:         "abc",
-						Repository:        "oci://localhost:5000/aaa",
-						VersionConstraint: "0.1.1",
-						Alias:             "abc-abc",
-					},
-				},
-			},
-			expected: false,
-		},
-		{
-			name: "existing dependency with empty item",
-			dep: unresolvedChartDependency{
-				ChartName:         "ghi",
-				Repository:        "oci://localhost:5000/aaa",
-				VersionConstraint: "0.1.0",
-				Alias:             "ghi-ghi",
-			},
-			deps: map[string][]unresolvedChartDependency{
-				"ghi": {},
-			},
-			expected: false,
-		},
-		{
-			name: "non-existing dependency",
-			dep: unresolvedChartDependency{
-				ChartName:         "def",
-				Repository:        "oci://localhost:5000/bbb",
-				VersionConstraint: "0.2.0",
-				Alias:             "def-def",
-			},
-			deps: map[string][]unresolvedChartDependency{
-				"abc": {
-					{
-						ChartName:         "abc",
-						Repository:        "oci://localhost:5000/aaa",
-						VersionConstraint: "0.1.0",
-						Alias:             "abc-abc",
-					},
-				},
-			},
-			expected: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &UnresolvedDependencies{
-				deps: tt.deps,
-			}
-			actual := d.contains(tt.dep)
-			require.Equal(t, tt.expected, actual)
 		})
 	}
 }
