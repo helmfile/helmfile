54059dbc4044c7ca93b6c85063a0536bbe69b2af
Vincent Chenal <vincent.chenal@protonmail.com>
2024-10-28 14:00:21 +0100

    feat: add environments values inheritance
    Signed-off-by: Vincent Chenal <vincent.chenal@protonmail.com>

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -89,6 +89,8 @@ type ReleaseSetSpec struct {
 	MissingFileHandlerConfig *MissingFileHandlerConfig `yaml:"missingFileHandlerConfig,omitempty"`
 
 	LockFile string `yaml:"lockFilePath,omitempty"`
+
+	Features []string `yaml:"features,omitempty"`
 }
 
 type MissingFileHandlerConfig struct {
