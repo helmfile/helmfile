2c2d83eb98742ebd4fb96ab1e7ff84f067648ab4
yxxhero <aiopsclub@163.com>
2024-11-13 08:48:13 +0800

    debug
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/test-cases/v1-subhelmfile-multi-bases-with-array-values.sh
+++ test/integration/test-cases/v1-subhelmfile-multi-bases-with-array-values.sh
@@ -9,7 +9,7 @@ if [[ ${HELMFILE_V1MODE} = true ]]; then
   info "Comparing v1 subhelmfile multi bases with array values output ${yaml_overwrite_reverse} with ${v1_subhelmfile_multi_bases_with_array_values_output_dir}/result"
   for i in $(seq 10); do
       info "Comparing build/v1-subhelmfile-multi-bases-with-array-values #$i"
-      ${helmfile} -f ${v1_subhelmfile_multi_bases_with_array_values_input_dir}/helmfile.yaml.gotmpl template -e dev &> ${yaml_overwrite_reverse} || (cat ${yaml_overwrite_reverse} && fail "\"helmfile template\" shouldn't fail")
+      ${helmfile} -f ${v1_subhelmfile_multi_bases_with_array_values_input_dir}/helmfile.yaml.gotmpl template --debug -e dev &> ${yaml_overwrite_reverse} || (cat ${yaml_overwrite_reverse} && fail "\"helmfile template\" shouldn't fail")
       diff -u ${v1_subhelmfile_multi_bases_with_array_values_output_dir}/result ${yaml_overwrite_reverse} || fail "\"helmfile template\" should be consistent"
       echo code=$?
   done
