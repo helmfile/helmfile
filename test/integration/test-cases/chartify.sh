ec5a5fdd75cffc2f2532d2bfe7348b18fd1376e9
Thomas Loubiou <xabufr@users.noreply.github.com>
2024-12-04 11:51:21 +0100

    fix: inject global values in Chartify (#1805)
    * fix: inject global values in Chartify
    
    Signed-off-by: Thomas Loubiou <thomas.loubiou@mirakl.com>
    
    * test: add integration tests for template --values
    
    Signed-off-by: Thomas Loubiou <thomas.loubiou@mirakl.com>
    
    ---------
    
    Signed-off-by: Thomas Loubiou <thomas.loubiou@mirakl.com>
    Signed-off-by: Justin Lai <justin.lai@invitae.com>

--- test/integration/test-cases/chartify.sh
+++ test/integration/test-cases/chartify.sh
@@ -12,16 +12,18 @@ fi
 chartify_tmp=$(mktemp -d)
 chartify_template_reverse=${chartify_tmp}/chartify.template.log
 
-
 test_start "helmfile template with chartify"
 
 info "Comparing template/chartify"
-${helmfile} -f ${chartify_case_input_dir}/${config_file} template > ${chartify_template_reverse} || fail "\"helmfile template\" shouldn't fail"
+${helmfile} -f ${chartify_case_input_dir}/${config_file} template >${chartify_template_reverse} || fail "\"helmfile template\" shouldn't fail"
 ./dyff between -bs ${chartify_case_output_dir}/template ${chartify_template_reverse} || fail "\"helmfile template\" should be consistent"
 
 info "Comparing template/chartify with set"
-${helmfile} -f ${chartify_case_input_dir}/${config_file} template --set image.tag=v2 > ${chartify_template_reverse} || fail "\"helmfile template\" shouldn't fail"
+${helmfile} -f ${chartify_case_input_dir}/${config_file} template --set image.tag=v2 >${chartify_template_reverse} || fail "\"helmfile template\" shouldn't fail"
 ./dyff between -bs ${chartify_case_output_dir}/template-set ${chartify_template_reverse} || fail "\"helmfile template\" should be consistent"
 
+info "Comparing template/chartify with values"
+${helmfile} -f ${chartify_case_input_dir}/${config_file} template --values "./extra-values.yaml" >${chartify_template_reverse} || fail "\"helmfile template\" shouldn't fail"
+./dyff between -bs ${chartify_case_output_dir}/template-values ${chartify_template_reverse} || fail "\"helmfile template\" should be consistent"
 
 test_pass "helmfile template with chartify"
