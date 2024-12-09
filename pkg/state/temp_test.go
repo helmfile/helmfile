d8e8c0f21472284814f34ecbdf81b7f0d71bc632
yxxhero <aiopsclub@163.com>
2024-12-09 23:12:42 +0800

    tests: fix more tests
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/state/temp_test.go
+++ pkg/state/temp_test.go
@@ -38,39 +38,39 @@ func TestGenerateID(t *testing.T) {
 	run(testcase{
 		subject: "baseline",
 		release: ReleaseSpec{Name: "foo", Chart: "incubator/raw"},
-		want:    "foo-values-5db58595d7",
+		want:    "foo-values-5b58697694",
 	})
 
 	run(testcase{
 		subject: "different bytes content",
 		release: ReleaseSpec{Name: "foo", Chart: "incubator/raw"},
 		data:    []byte(`{"k":"v"}`),
-		want:    "foo-values-78d88d86dd",
+		want:    "foo-values-58bff47d77",
 	})
 
 	run(testcase{
 		subject: "different map content",
 		release: ReleaseSpec{Name: "foo", Chart: "incubator/raw"},
 		data:    map[string]any{"k": "v"},
-		want:    "foo-values-f9c8967cd",
+		want:    "foo-values-5fb8948f75",
 	})
 
 	run(testcase{
 		subject: "different chart",
 		release: ReleaseSpec{Name: "foo", Chart: "stable/envoy"},
-		want:    "foo-values-cdfb97444",
+		want:    "foo-values-784b76684f",
 	})
 
 	run(testcase{
 		subject: "different name",
 		release: ReleaseSpec{Name: "bar", Chart: "incubator/raw"},
-		want:    "bar-values-749bc4c6d4",
+		want:    "bar-values-f48df5f49",
 	})
 
 	run(testcase{
 		subject: "specific ns",
 		release: ReleaseSpec{Name: "foo", Chart: "incubator/raw", Namespace: "myns"},
-		want:    "myns-foo-values-7b74fbd6d6",
+		want:    "myns-foo-values-6b68696b8c",
 	})
 
 	for id, n := range ids {
