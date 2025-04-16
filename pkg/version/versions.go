64256ecec1a42d2050abc075272502c0c859cd15
Hubertbits <170125456+hubertbits@users.noreply.github.com>
2025-04-16 19:01:38 +0200

    Update Helm version constants after rebase
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/version/versions.go
+++ pkg/version/versions.go
@@ -2,10 +2,10 @@ package version
 
 const (
 	HelmRequiredVersion           = "v3.16.4"
-	HelmRecommendedVersion        = "v3.17.2"
+	HelmRecommendedVersion        = "v3.17.3"
 	HelmDiffRecommendedVersion    = "v3.11.0"
-	HelmSecretsRecommendedVersion = "v4.6.0"
-	HelmGitRecommendedVersion     = "v0.15.1"
-	HelmS3RecommendedVersion      = "v0.16.0"
+	HelmSecretsRecommendedVersion = "v4.6.3"
+	HelmGitRecommendedVersion     = "v1.3.0"
+	HelmS3RecommendedVersion      = "v0.16.3"
 	HelmInstallCommand            = "https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
 )
