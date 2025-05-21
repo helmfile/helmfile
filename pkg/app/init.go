e197a90597477ef1d072bdfef61286dd874553c1
yxxhero <11087727+yxxhero@users.noreply.github.com>
2025-05-21 16:57:36 +0800

    build(helm) update to v3.18.0 (#2044)
    * build(helm) update to v3.18.0
    
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- pkg/app/init.go
+++ pkg/app/init.go
@@ -17,10 +17,10 @@ import (
 )
 
 const (
-	HelmRequiredVersion           = "v3.16.4"
-	HelmRecommendedVersion        = "v3.17.3"
+	HelmRequiredVersion           = "v3.17.3"
+	HelmRecommendedVersion        = "v3.18.0"
 	HelmDiffRecommendedVersion    = "v3.11.0"
-	HelmSecretsRecommendedVersion = "v4.6.3"
+	HelmSecretsRecommendedVersion = "v4.6.5"
 	HelmGitRecommendedVersion     = "v1.3.0"
 	HelmS3RecommendedVersion      = "v0.16.3"
 	HelmInstallCommand            = "https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
