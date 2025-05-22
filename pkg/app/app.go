e3370fc1759cc6da02cc46910a5d603e45cfedb1
Raphael Luba <raphael@leanbyte.com>
2025-05-22 10:26:36 +0200

    sync: Emit "cleanup" events later to match the behavior in "apply"

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2175,13 +2175,6 @@ func (a *App) sync(r *Run, c SyncConfigProvider) (bool, []error) {
 		}
 	}
 
-	for id := range releasesWithNoChange {
-		r := releasesWithNoChange[id]
-		if _, err := st.TriggerCleanupEvent(&r, "sync"); err != nil {
-			a.Logger.Warnf("warn: %v\n", err)
-		}
-	}
-
 	names := []string{}
 	for _, r := range releasesToUpdate {
 		names = append(names, fmt.Sprintf("  %s (%s) UPDATED", r.Name, r.Chart))
@@ -2279,6 +2272,13 @@ Do you really want to sync?
 	}
 
 	affectedReleases.DisplayAffectedReleases(c.Logger())
+
+	for id := range releasesWithNoChange {
+		r := releasesWithNoChange[id]
+		if _, err := st.TriggerCleanupEvent(&r, "sync"); err != nil {
+			a.Logger.Warnf("warn: %v\n", err)
+		}
+	}
 
 	return true, errs
 }
