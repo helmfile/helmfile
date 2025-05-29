6c0347a948fe15bb471f741e46801799d46e5888
yxxhero <aiopsclub@163.com>
2025-05-29 21:47:42 +0800

    fix more tests
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/test-cases/chartify-with-non-chart-dir.sh
+++ test/integration/test-cases/chartify-with-non-chart-dir.sh
@@ -17,9 +17,12 @@ test_start "$case_title"
 info "Comparing ${case_title} diff for output ${chartify_with_non_chart_dirt_reverse} with ${diff_out_file}"
 for i in $(seq 10); do
     info "Comparing chartify-with-non-chart-dir diff log #$i"
-    ${helmfile} -f ${chartify_with_non_chart_dirt_input_dir}/helmfiles/helmfile.yaml diff 2>&1 | grep -vE "^(Comparing release|Building dependency release)" > ${chartify_with_non_chart_dirt_reverse} || fail "\"helmfile diff\" shouldn't fail"
+    ${helmfile} -f ${chartify_with_non_chart_dirt_input_dir}/helmfiles/helmfile.yaml diff &> ${chartify_with_non_chart_dirt_reverse} || fail "\"helmfile diff\" shouldn't fail"
+    cat ${chartify_with_non_chart_dirt_reverse} | grep -vE "^(Comparing release|Building dependency release)" > ${chartify_with_non_chart_dirt_reverse} 
+
     cat ${diff_out_file}
     cat ${chartify_with_non_chart_dirt_reverse}
+
     diff -u ${diff_out_file} ${chartify_with_non_chart_dirt_reverse} || fail "\"helmfile diff\" should be consistent"
     echo code=$?
 done
