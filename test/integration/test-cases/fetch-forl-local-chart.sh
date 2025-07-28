96b2732799c4079f0488c13b5765cd8a75af93bb
yxxhero <aiopsclub@163.com>
2025-07-28 13:29:04 +0800

    fix(test): update path in fetch-forl-local-chart test
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/test-cases/fetch-forl-local-chart.sh
+++ test/integration/test-cases/fetch-forl-local-chart.sh
@@ -8,7 +8,7 @@ test_start "$case_title"
 
 info "Comparing fetch-forl-local-chart diff log #$i"
 ${helmfile} -f ${fetch_forl_local_chart_input_dir}/helmfile.yaml.gotmpl fetch --output-dir ${fetch_forl_local_chart_tmp} || fail "\"helmfile fetch\" shouldn't fail"
-cat ${fetch_forl_local_chart_tmp}/local-chart/local-chart/raw/latest/Chart.yaml || fail "Chart.yaml should exist in the fetched local chart directory"
+cat ${fetch_forl_local_chart_tmp}/helmfile-tests/local-chart/raw/latest/Chart.yaml || fail "Chart.yaml should exist in the fetched local chart directory"
 echo code=$?
 
 test_pass "$case_title"
