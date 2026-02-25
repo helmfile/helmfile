546c4068245721b06da0ccda2aca3f67f7a95543
Aditya Menon <amenon@canarytechnologies.com>
2026-02-25 20:18:44 +0530

    fix: use absolute baseDir in sequential helmfiles for correct values path resolution (#2424)
    PR #2410 introduced a regression where a relative directory was passed as
    baseDir instead of an absolute one, causing values and secrets file paths
    to resolve incorrectly when using --sequential-helmfiles with helmfile.d/.
    
    Signed-off-by: Aditya Menon <amenon@canarytechnologies.com>

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -133,6 +133,7 @@ ${kubectl} create namespace ${test_ns} || fail "Could not create namespace ${tes
 . ${dir}/test-cases/issue-2409-sequential-kubecontext.sh
 . ${dir}/test-cases/issue-2269.sh
 . ${dir}/test-cases/issue-2418.sh
+. ${dir}/test-cases/issue-2424-sequential-values-paths.sh
 
 # ALL DONE -----------------------------------------------------------------------------------------------------------
 
