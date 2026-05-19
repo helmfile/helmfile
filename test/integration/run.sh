5e3d8a17fdae0df419061fcb8709b56212d3f355
yxxhero <aiopsclub@163.com>
2026-05-20 07:07:43 +0800

    fix: normalize dependency chart path before DirectoryExistsAt check
    When helmfile.d contains multiple release files and one release has a
    local chart dependency (e.g. chart: ../chart), the dependency path was
    passed to DirectoryExistsAt without normalizing against basePath. This
    caused the path to be resolved against CWD instead of the helmfile
    directory, so the local chart was not detected and helmfile tried to
    resolve it as a remote repo, failing with:
      'failed reading adhoc dependencies: no helm list entry found for repository'
    
    Fixes #2596
    
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -143,6 +143,7 @@ ${kubectl} create namespace ${test_ns} || fail "Could not create namespace ${tes
 . ${dir}/test-cases/issue-2424-sequential-values-paths.sh
 . ${dir}/test-cases/issue-2431.sh
 . ${dir}/test-cases/issue-2544.sh
+. ${dir}/test-cases/issue-2596-local-deps-multiple-files.sh
 . ${dir}/test-cases/kubedog-tracking.sh
 
 # ALL DONE -----------------------------------------------------------------------------------------------------------
