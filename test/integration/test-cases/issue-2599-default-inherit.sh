d0e9b7ec605a39fe810f8cd7fd9257c864be90d1
yxxhero <aiopsclub@163.com>
2026-05-20 08:24:36 +0800

    fix: use dir variable for chart path
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- test/integration/test-cases/issue-2599-default-inherit.sh
+++ test/integration/test-cases/issue-2599-default-inherit.sh
@@ -39,7 +39,7 @@ cat > ${issue_2599_tmp}/bad-helmfile.yaml <<EOF
 defaultInherit: nonexistent
 releases:
 - name: app1
-  chart: ${cases_dir}/../charts/raw
+  chart: ${dir}/charts/raw
 EOF
 
 ${helmfile} -f ${issue_2599_tmp}/bad-helmfile.yaml template \
